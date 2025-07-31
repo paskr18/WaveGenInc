21a76a7eb2e8ae554e606215244bbe1d7960937c
paskr18 <146536178+paskr18@users.noreply.github.com>
2025-07-31 08:46:51 -0400

    Add files via upload

---
+++ navigation_menu.php
@@ -0,0 +1,13 @@
+<?php
+  echo '<div class="navbar">';
+  echo '<a id = "company_overview" href = "company_overview.php"> Company Overview  </a>';
+  echo '<a id = "past_research_work" href = "past_research_work.php"> Past Research Work </a>';
+  echo '<a id = "collaboration" href = "collaboration.php"> WaveGen <> M2M Tech Collaboration ►</a>';
+  echo '<a id = "EDA" href = "EDA.php"> Exploratory Data Analysis (EDA) ►</a>';
+  echo '<a id = "Nemoh" href = "nemoh.php"> Nemoh Simulations</a>';
+  echo '<a id = "XGBoost" href = "XGBoost.php"> Machine Learning - XGBoost</a>';
+  echo '<a id = "LightGBM"> (TBD) Machine Learning - LightGBM</a>';
+  echo '<a id = "assumptions_limitations"> Assumptions-Limitations</a>';
+  echo '<a id = "recommendations"> Future Recommendations</a>';
+  echo '</div>';
+?>
---
+++ past_research_work.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="fr-FR">
+  <head>
+    <link rel="stylesheet" type="text/css" href="wavegen.css">
+    <meta name="author" content="Salnave Kenny Robert Philippe-Auguste">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+
+  <body>
+   <?php include("navigation_menu.php") ?>  
+    <script>
+        const myActive = document.getElementById("past_research_work");
+        myActive.style.backgroundColor="#000080";
+        myActive.style.color="#FFFFFF";
+     </script>
+     <div class = "contenu">
+	<h1 style = "width:400px;"> <img src = "images/wavegen_logo.png" style = "float:right; width:100px;"> WaveGen Tech Inc. </h1>
+        <h2> Past Research Work </h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sapien turpis, consectetur nec luctus a, egestas sed diam. Vestibulum tincidunt arcu sed nisl hendrerit, sagittis mattis tellus posuere. Nunc quis ipsum sagittis, commodo mauris et, ultrices enim. Donec fermentum nibh lorem, vel commodo ligula consequat at. Aliquam vestibulum dolor vitae metus sollicitudin, vel lobortis ante tristique. Sed porta pharetra lorem non faucibus. Donec gravida erat sapien, a accumsan nisl commodo id. Vivamus consequat est vel scelerisque commodo. Aenean at dapibus lectus. Fusce ac efficitur ex. Nulla in nulla non dui egestas pulvinar ac at odio. Nullam sed lacus non ante sagittis gravida. Donec tincidunt dui sit amet diam porta, a commodo augue dictum. Quisque in felis iaculis, finibus nisi quis, vehicula tellus. </p>
+     </div>
+  </body>
+</html>
