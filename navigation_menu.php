91742cd56818c808d0b0031c38c503f98b757ce8
paskr18 <146536178+paskr18@users.noreply.github.com>
2025-08-04 12:51:20 -0400

    Add files via upload

--- navigation_menu.php
+++ navigation_menu.php
@@ -3,11 +3,17 @@
   echo '<a id = "company_overview" href = "company_overview.php"> Company Overview  </a>';
   echo '<a id = "past_research_work" href = "past_research_work.php"> Past Research Work </a>';
   echo '<a id = "collaboration" href = "collaboration.php"> WaveGen <> M2M Tech Collaboration ►</a>';
-  echo '<a id = "EDA" href = "EDA.php"> Exploratory Data Analysis (EDA) ►</a>';
+  echo '<div class="dropdown">';
+  echo '<a id = "EDA"> Exploratory Data Analysis (EDA) &#9660</a>';
+  echo '<div class="dropdown-content">';
+  echo '<a href="EDA.php"> Phase 1 </a>';
+  echo '<a href="EDA.php"> Phase 2 </a>';
+  echo '</div>';
+  echo '</div>';
   echo '<a id = "Nemoh" href = "nemoh.php"> Nemoh Simulations</a>';
   echo '<a id = "XGBoost" href = "XGBoost.php"> Machine Learning - XGBoost</a>';
-  echo '<a id = "LightGBM"> (TBD) Machine Learning - LightGBM</a>';
-  echo '<a id = "assumptions_limitations"> Assumptions-Limitations</a>';
-  echo '<a id = "recommendations"> Future Recommendations</a>';
+  echo '<a id = "LightGBM" href = "LightGBM.php"> (TBD) Machine Learning - LightGBM</a>';
+  echo '<a id = "assumptions_limitations" href = "assumptions_limitations.php"> Assumptions - Limitations</a>';
+  echo '<a id = "recommendations" href = "recommendations.php"> Future Recommendations</a>';
   echo '</div>';
 ?>
